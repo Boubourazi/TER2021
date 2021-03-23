638acd085b453da3225c82e6fa8c62798302b0c9
Boubourazi <aurelien.llorca@gmail.com>
2021-03-24 00:26:24 +0100

    refactored future gestion

--- app_ter/lib/main.dart
+++ app_ter/lib/main.dart
@@ -72,84 +72,71 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, //Color(0xFF0b75b0),
-      key: this._scaffoldKey,
-      resizeToAvoidBottomInset: false,
-      drawer: CustomDrawer(),
-      appBar: AppBar(
-        centerTitle: true,
-        toolbarHeight: 50,
-        backgroundColor: Color(0xFF0b75b0),
-        elevation: 0,
-        shadowColor: Colors.blue,
-        title: Text(
-          widget.title,
-          style: Theme.of(context).textTheme.headline1,
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            this.openDrawer();
-          },
-        ),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(
-                Icons.map,
+    return FutureBuilder(
+        future: this._connecter.initialize().then((value) =>
+            this._connecter.db.collection("commerces").find().toList()),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          return Scaffold(
+            backgroundColor: Colors.white, //Color(0xFF0b75b0),
+            key: this._scaffoldKey,
+            resizeToAvoidBottomInset: false,
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              centerTitle: true,
+              toolbarHeight: 50,
+              backgroundColor: Color(0xFF0b75b0),
+              elevation: 0,
+              shadowColor: Colors.blue,
+              title: Text(
+                widget.title,
+                style: Theme.of(context).textTheme.headline1,
               ),
-              onPressed: () {}),
-        ],
-      ),
-      body: Stack(
-        children: <Widget>[
-          FutureBuilder(
-            future: this._connecter.initialize().then((value) =>
-                this._connecter.db.collection("commerces").find().toList()),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                        backgroundColor: Colors.blue[200],
-                        strokeWidth: 3,
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  this.openDrawer();
+                },
+              ),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(
+                      Icons.map,
+                    ),
+                    onPressed: () {}),
+              ],
+            ),
+            body: Stack(
+              children: <Widget>[
+                snapshot.connectionState == ConnectionState.done
+                    ? Map(snapshot.data)
+                    : Center(
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.blue),
+                          backgroundColor: Colors.blue[200],
+                          strokeWidth: 3,
+                        ),
+                      ),
+                ClipPath(
+                  clipper: RoundedBottom(),
+                  child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFF0b75b0),
+                          Color(0xFF0eaddc),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
                       ),
                     ),
                   ),
-                );
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                return snapshot.hasData ? Map(snapshot.data) : Text("Pending");
-              }
-              if (snapshot.hasError) {
-                return Text("Error while accessing the database");
-              }
-              //Will never reach this point
-              return Text("Coucou moi c'est Aurélien");
-            },
-          ),
-          ClipPath(
-            clipper: RoundedBottom(),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF0b75b0),
-                    Color(0xFF0eaddc),
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
                 ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
+        });
   }
 }
